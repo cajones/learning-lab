4af1ca531e7fa38ac0e2fbd9a36d790f55c60f08
Chris Jones <chrisjones0099@hotmail.com>
2012-01-25 09:31:12 +0000

    custom port

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var http = require('http');
+
+var hosting = {
+	port : process.argv[2] || 80
+};
+console.log(hosting.port);
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Goodbye Cruel World\n');
-}).listen(1337);
+}).listen(hosting.port);
