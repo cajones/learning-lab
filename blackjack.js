06fbe9aaeea1d89755c29af538bc21f1966274c2
Peter Bailey <pete@psbailey.co.uk>
2012-01-27 12:55:27 +0000

    Fixed runtime errors

--- blackjack.js
+++ blackjack.js
@@ -1,9 +1,9 @@
 
 exports.Hand = function(){
 
-var values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-var suits = ['Hearts', 'Diamonds'', 'Clubs', 'Spades']
+  var values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+  var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 
-this.cards = ["3 diamonds", "6 clubs"];
+  this.cards = ['3 diamonds', '6 clubs'];
 
 };
